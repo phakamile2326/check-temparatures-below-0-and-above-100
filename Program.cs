d50f7ddf9fcfbfa7425367bef910ff002b3dd196
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-17 05:49:51 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check_temparatures_below_0_and_above_100
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(check(120, -1));
+            Console.WriteLine(check(-1,120));
+            Console.WriteLine(check(2,120));
+            Console.ReadKey();
+
+        }
+        public static bool check(int temp1, int temp2)
+        {
+            return temp1 < 0 && temp2 > 100 || temp2 < 0 && temp1 > 100;
+        }
+            
+    }
+}
